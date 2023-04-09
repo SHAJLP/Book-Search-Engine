b4e46fa8f9a0dde6e141ee49914a2e4d92e7fe47
sophia altaf <Sophia@sophias-mbp.home>
2023-04-09 17:30:49 +0100

    setting up mutation.js file

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import { gql } from "@appollo/client";
+
+export const LOGIN = gql`
+mutation Mutation($input; LoginInput!){
+    login(input: $input){
+        token
+    
+    }
+}
+`;
+export const SIGNUP  = gql`
+mutation Mutation ($input: SignupInput!) {
+signup (input: $input)  {
+    token
+}  
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation Mutation ($input; SaveBookInput!) {
+    saveBook (input: $input) {
+        id
+        username
+        email
+        bookcount
+        savedBooks{
+            authors
+            description
+            bookId
+            image 
+            title
+            link
+        }
+        }
+    }
+`;
+export const REMOVE_BOOK = gql`
+mutation Mutation ($bookId: String!) {
+    removeBook (bookId: $bookId){
+        id
+        username
+        email
+        bookcount
+        savedBooks{
+            authors
+            description
+            bookId
+            image 
+            title
+            link
+        }
+    }
+}
+`;
