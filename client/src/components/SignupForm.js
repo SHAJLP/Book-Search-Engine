1759dc59840ef0caed3516c1725c6b111a65cecb
sophia altaf <Sophia@sophias-mbp.home>
2023-04-10 16:44:43 +0100

    initial signup.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import {useMutation} from "@appollo/client";
+import {SIGNUP}from "..utils/mutations";
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
   // set state for form validation
   const [validated] = useState(false);
+
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  // Function to "SIGNUP" returned by UseMutation hook
+  const (signup) = useMutation (SIGNUP);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,18 +34,17 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
+      const {data} = await signup({
+        variables: {
+          input: userFormData,
+        },
+      });
+      Auth.login(data.signup.token);
+    }catch (err) {
+      console.error (err);
       setShowAlert(true);
+    }
+
     }
 
     setUserFormData({
